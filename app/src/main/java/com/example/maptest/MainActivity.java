f3e5065925238298ebf2670e8ac80a8d2a7f05ca
valsora <YB9173739847@mail.ru>
2022-04-03 22:11:54 +0500

    record gotov

--- app/src/main/java/com/example/maptest/MainActivity.java
+++ app/src/main/java/com/example/maptest/MainActivity.java
@@ -26,40 +26,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start(View view) {
-        SQLiteDatabase rdb = rdbHelper.getWritableDatabase();
-        float distance = (float) Math.random() * 100;
-        int time = (int) (Math.random() * 10000);
-        int day = (int) (Math.random() * 32);
-        int month = (int) (Math.random() * 13);
-        String date = day + "." + month + ".22";
-        ContentValues cvrdb = new ContentValues();
-        cvrdb.put(RecordsContract.ClassForRecords.column_distance, distance);
-        cvrdb.put(RecordsContract.ClassForRecords.column_time, time);
-        cvrdb.put(RecordsContract.ClassForRecords.column_date, date);
-        rdb.insert(RecordsContract.ClassForRecords.table_name, null, cvrdb);
-
-        SQLiteDatabase rdb2 = rdbHelper.getReadableDatabase();
-        String[] columns = {
-                RecordsContract.ClassForRecords._id
-        };
-        Cursor cursor = rdb2.query(
-                RecordsContract.ClassForRecords.table_name,
-                columns, null, null, null, null, null
-        );
-        cursor.moveToLast();
-        int recordId = cursor.getInt(0);
-        cursor.close();
-
-        SQLiteDatabase pdb = pdbHelper.getWritableDatabase();
-        for (int i = 0; i < 7; i++) {
-            double latitude = Math.random() * 50;
-            double longitude = Math.random() * 50;
-            ContentValues cvpdb = new ContentValues();
-            cvpdb.put(PointsContract.ClassForPoints.column_latitude, latitude);
-            cvpdb.put(PointsContract.ClassForPoints.column_longitude, longitude);
-            cvpdb.put(PointsContract.ClassForPoints.column_recordId, recordId);
-            pdb.insert(PointsContract.ClassForPoints.table_name, null, cvpdb);
-        }
         Intent intent = new Intent(MainActivity.this, MapActivity.class);
         startActivity(intent);
     }
