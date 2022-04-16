08d9e8f5804a840f0ff732e5b033a49fbbfab127
FutRel <gxrim@bk.ru>
2022-04-16 21:21:32 +0500

    123

--- app/src/main/java/com/example/maptest/MapInformationActivity.java
+++ app/src/main/java/com/example/maptest/MapInformationActivity.java
@@ -1,13 +1,10 @@
 package com.example.maptest;
 
-import android.app.Service;
-import android.bluetooth.BluetoothClass;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.view.View;
-import android.widget.TimePicker;
 import android.widget.Toast;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
@@ -100,13 +97,23 @@ public class MapInformationActivity extends FragmentActivity implements OnMapRea
         if (arrayLatitude.size() > 0) {
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(arrayLatitude.get(arrayLatitude.size() - 2),
                     arrayLongitude.get(arrayLongitude.size() - 2)), 16));
+
             PolylineOptions polylineOptions = new PolylineOptions().color(Color.RED);
+            PolylineOptions polylineOptionsGrey = new PolylineOptions().color(Color.GRAY);
+
+            if (arrayLatitude.get(arrayLatitude.size() - 1) == 0.0){
+                arrayLatitude.remove(arrayLatitude.size() - 1);
+                arrayLongitude.remove(arrayLongitude.size() - 1);
+            }
             for (int i = 1; i < arrayLatitude.size(); i++) {
                 if (arrayLatitude.get(i) != 0.0)
                     polylineOptions.add(new LatLng(arrayLatitude.get(i), arrayLongitude.get(i)));
                 else {
                     mMap.addPolyline(polylineOptions);
                     polylineOptions = new PolylineOptions().color(Color.RED);
+                    polylineOptionsGrey.add(new LatLng(arrayLatitude.get(i - 1), arrayLongitude.get(i - 1)), new LatLng(arrayLatitude.get(i + 1), arrayLongitude.get(i + 1)));
+                    mMap.addPolyline(polylineOptionsGrey);
+                    polylineOptionsGrey = new PolylineOptions().color(Color.GRAY);
                 }
             }
             mMap.addPolyline(polylineOptions);
