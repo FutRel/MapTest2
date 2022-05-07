40686fe55e4c36e2e93ca2d222568cb752cbc43c
FutRel <gxrim@bk.ru>
2022-05-08 00:14:17 +0500

    123

--- app/src/main/java/com/example/maptest/MainActivity.java
+++ app/src/main/java/com/example/maptest/MainActivity.java
@@ -1,27 +1,52 @@
 package com.example.maptest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.view.View;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     static boolean threadBool = true;
+    private static final int ACCESS_COARSE_LOCATION = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission(ACCESS_COARSE_LOCATION);
     }
 
     public void start(View view) {
-        Intent intent = new Intent(MainActivity.this, MapActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(MainActivity.this, MapActivity.class));
     }
 
     public void intentToLV(View view){
-        Intent intent = new Intent(MainActivity.this, RecyclerViewActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(MainActivity.this, RecyclerViewActivity.class));
+    }
+
+    public void checkPermission(int requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED ||
+                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, requestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == ACCESS_COARSE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) Toast.makeText(MainActivity.this,
+                    "Permission Granted", Toast.LENGTH_SHORT) .show();
+            else Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT) .show();
+        }
     }
 }
